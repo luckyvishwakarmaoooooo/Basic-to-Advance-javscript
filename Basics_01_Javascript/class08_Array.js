66baf19ee85578db20b3068051e273a3edb561d4
luckyvishwakarmaoooooo <144231513+luckyvishwakarmaoooooo@users.noreply.github.com>
2025-01-13 16:50:55 +0000

    incomplete arrray

--- Basics_01_Javascript/class08_Array.js
+++ Basics_01_Javascript/class08_Array.js
@@ -1,28 +1,39 @@
-// Definaton OF Array
-// Array is Data structure that store collection of elements in a single veriable where each elements
-// is accessable by using index number 
+// // Definaton OF Array
+// // Array is Data structure that store collection of elements in a single veriable where each elements
+// // is accessable by using index number 
 
-// Important javascript array methods-
+// // Important javascript array methods-
 
-// 1 push method =>Adds one or more elements to the end of the array;
-let arr = [2,4,6];
-console.log(arr.push(8),arr);
+// // 1 push method =>Adds one or more elements to the end of the array;
+// let arr = [2,4,6];
+// console.log(arr.push(8),arr);
 
-// 2 pop method => remove one element to the end of the array and return it
-let arr2 = [5,569,89,88,78];
-console.log(arr2.pop(),arr2);
-console.log(arr2)
+// // 2 pop method => remove one element to the end of the array and return it
+// let arr2 = [5,569,89,88,78];
+// console.log(arr2.pop(),arr2);
+// console.log(arr2)
 
-// 3 shift method => remove first elements from array and returns it 
+// // 3 shift method => remove first elements from array and returns it 
 
-let arr3 = ["aman","aditya","hariom","dhammu"];
-console.log(arr3.shift(),arr3);
+// let arr3 = ["aman","aditya","hariom","dhammu"];
+// console.log(arr3.shift(),arr3);
 
-// 4 unshift method => add one more element at the begining  of an array 
-let arr4 = [1,2,"mart"];
-console.log(arr4.unshift("D-"),arr4);
+// // 4 unshift method => add one more element at the begining  of an array 
+// let arr4 = [1,2,"mart"];
+// console.log(arr4.unshift("D-"),arr4);
 
-// 5 concat method=> merege two or more array into one ;
-console.log(arr.concat(arr2,arr3));
+// // 5 concat method=> merege two or more array into one ;
+// console.log(arr.concat(arr2,arr3));
 
-// start form slice method 
+// // start form slice method 
+
+// let arr5 = ['css','react','node','javascript','database']
+// console.log(arr5.slice(1,5));
+// console.log(arr5)
+
+// splice method
+// let arr5 = ['css','react','node','javascript','database']
+// console.log(arr5.splice(1,2,"JAVA","RUBY"))
+// console.log(arr5)
+
+
