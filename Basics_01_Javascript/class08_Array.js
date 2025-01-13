d21936e9652f0aa87cbfc8d9e02f084293ef018b
luckyvishwakarmaoooooo <144231513+luckyvishwakarmaoooooo@users.noreply.github.com>
2025-01-13 03:46:32 +0000

    Incomplete Array topic

---
+++ Basics_01_Javascript/class08_Array.js
@@ -0,0 +1,28 @@
+// Definaton OF Array
+// Array is Data structure that store collection of elements in a single veriable where each elements
+// is accessable by using index number 
+
+// Important javascript array methods-
+
+// 1 push method =>Adds one or more elements to the end of the array;
+let arr = [2,4,6];
+console.log(arr.push(8),arr);
+
+// 2 pop method => remove one element to the end of the array and return it
+let arr2 = [5,569,89,88,78];
+console.log(arr2.pop(),arr2);
+console.log(arr2)
+
+// 3 shift method => remove first elements from array and returns it 
+
+let arr3 = ["aman","aditya","hariom","dhammu"];
+console.log(arr3.shift(),arr3);
+
+// 4 unshift method => add one more element at the begining  of an array 
+let arr4 = [1,2,"mart"];
+console.log(arr4.unshift("D-"),arr4);
+
+// 5 concat method=> merege two or more array into one ;
+console.log(arr.concat(arr2,arr3));
+
+// start form slice method 
